464667a7c11871a67a9a7fe5530c11734a0280d8
ahmadsaghir <ahmad.saghir.1999@gmail.com>
2021-05-30 15:25:35 +0300

    CASE-13-F6-RANDEVULAR ROUTING

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,12 @@ Route::get('/hakkimizda', 'Controller@hakkimizda');
 Route::get('/iletisim', 'Controller@iletisim');
 Route::get('/otomasyon', 'Controller@otomasyon');
 
+/*randevu*/
+Route::get('/otomasyon/randevular/create', 'RandevuController@create');
+Route::post('/otomasyon/randevular', 'RandevuController@store');
+Route::get('/otomasyon/randevular', 'RandevuController@index')->name('randevu.index');
+Route::get('/otomasyon/randevular/{randevu}', 'RandevuController@show')->name('randevu.show');
+Route::get('/otomasyon/randevular/{randevu}/edit', 'RandevuController@edit');
+Route::put('/otomasyon/randevular/{randevu}', 'RandevuController@update');
+Route::get('/otomasyon/randevular/{randevu}/delete', 'RandevuController@destroy')->name('randevu.delete');
+Route::get('/randevular/search/', 'RandevuController@search')->name('randevu.search');
